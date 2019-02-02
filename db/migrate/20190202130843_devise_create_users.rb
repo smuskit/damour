b01a8002d0544d965c58f2bc946b814e7e70c3f3
smuskit <k.shimouse@gmail.com>
2019-02-02 13:18:26 +0000

    add calumn to Users

--- db/migrate/20190202130843_devise_create_users.rb
+++ db/migrate/20190202130843_devise_create_users.rb
@@ -32,7 +32,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
+      t.string :name
+      t.string :name_kana
+      t.string :hundle_name
+      t.string :email
+      t.string :profile_image_id
+      t.boolean :deleted_at
       t.timestamps null: false
     end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_02_02_130843) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "name"
+    t.string "name_kana"
+    t.string "hundle_name"
+    t.string "profile_image_id"
+    t.boolean "deleted_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
 end
