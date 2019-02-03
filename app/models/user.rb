e36901fc6daabe5be84d5d5b54d88a93fadcf4b3
smuskit <k.shimouse@gmail.com>
2019-02-03 06:45:56 +0000

    create JapanMap to top_page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-<<<<<<< HEAD
   has_many :cards, dependent: :destroy
-=======
+
   attachment :profile_image
->>>>>>> 3e0dfa988e16dee9088a73097dde608705c811ca
+
 end
