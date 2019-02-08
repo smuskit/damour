241427d4fee99d43df84895d7d773783d5ea29ba
smuskit <k.shimouse@gmail.com>
2019-02-08 03:36:49 +0000

    Sort in alphabetical order prefectures#show

--- app/controllers/prefectures_controller.rb
+++ app/controllers/prefectures_controller.rb
@@ -2,6 +2,6 @@ class PrefecturesController < ApplicationController
   def show
     @prefecture = Prefecture.find(params[:id])
     @region = @prefecture.region
-    @dams = @prefecture.dams.all
+    @dams = @prefecture.dams.all.order :name_kana
   end
 end
