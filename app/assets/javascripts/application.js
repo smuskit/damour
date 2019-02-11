18d140d5483e31bc12c07686b135007deceb167e
smuskit <k.shimouse@gmail.com>
2019-02-11 06:36:10 +0000

    anchor scroll in top page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -90,4 +90,17 @@ $(function(){
 });
 
 
-//ナビバー
+//anchor
+$(function(){
+  $('a[href^="#"]').click(function() {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $('body,html').animate({scrollTop:position}, 500, 'swing');
+    return false;
+  });
+});
+
+
+
+
--- app/controllers/prefectures_controller.rb
+++ app/controllers/prefectures_controller.rb
@@ -2,7 +2,7 @@ class PrefecturesController < ApplicationController
   def show
     if params[:search]
       prefecture = Prefecture.find(params[:id])
-      @dams = prefecture.dams.all.where('name_kana LIKE ?', "%#{params[:search]}%")
+      @dams = prefecture.dams.all.where('name LIKE ? OR name_kana LIKE ? OR address LIKE ?', "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%")
     else
       prefecture = Prefecture.find(params[:id])
       @dams = prefecture.dams.all.order :name_kana
