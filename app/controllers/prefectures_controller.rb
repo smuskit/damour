113fe6b975e9f485407e964a7c1b328c729ecef0
smuskit <k.shimouse@gmail.com>
2019-02-12 07:53:29 +0000

    add pagenation to prefectures#show

--- app/controllers/prefectures_controller.rb
+++ app/controllers/prefectures_controller.rb
@@ -2,10 +2,10 @@ class PrefecturesController < ApplicationController
   def show
     if params[:search]
       prefecture = Prefecture.find(params[:id])
-      @dams = prefecture.dams.all.where('name LIKE ? OR name_kana LIKE ? OR address LIKE ?', "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%")
+      @dams = prefecture.dams.page(params[:page]).where('name LIKE ? OR name_kana LIKE ? OR address LIKE ?', "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%")
     else
       prefecture = Prefecture.find(params[:id])
-      @dams = prefecture.dams.all.order :name_kana
+      @dams = prefecture.dams.page(params[:page]).order :name_kana
     end
 
     @prefecture = Prefecture.find(params[:id])
