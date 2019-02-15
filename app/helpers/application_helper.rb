d7611a375b46eaa16ed47becb7eba218310d93fd
Your Name <you@example.com>
2019-02-15 17:21:20 +0900

    change devise error message to bootstrap version

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+  #ログイン
   def resource_name
    :user
   end
@@ -9,5 +10,22 @@ module ApplicationHelper
 
   def devise_mapping
      @devise_mapping ||= Devise.mappings[:user]
+  end
+  #ログイン
+
+
+  #devise エラーメッセージカスタメイズ
+  def devise_error_messages
+    return "" if resource.errors.empty?
+    html = ""
+    # エラーメッセージ用のHTMLを生成
+    messages = resource.errors.full_messages.each do |msg|
+      html += <<-EOF
+        <div class="error_field alert alert-danger" role="alert">
+          <p class="error_msg">#{msg}</p>
+        </div>
+      EOF
+    end
+    html.html_safe
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/users', to: redirect("/users/sign_up")
 
   root to: 'dams#top'
 
