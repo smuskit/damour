4cbde1c64329e6187ea974b231bd4306d5510530
Your Name <you@example.com>
2019-02-18 20:00:52 +0900

    finish inquiry page

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -5,4 +5,5 @@ class Inquiry < ApplicationRecord
 
   validates :name, :presence => {:message => '名前を入力してください'}
   validates :email, :presence => {:message => 'メールアドレスを入力してください'}
+  validates :message, :presence => {:message => 'お問い合わせ内容を入力してください'}
 end
