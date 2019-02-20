26f3838ae4d668ebe084cbcdd902ab4d103391a0
smuskit <k.shimouse@gmail.com>
2019-02-20 08:29:32 +0000

    Add two lines of text to the About page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,11 +29,11 @@ document.addEventListener("turbolinks:load", function(){
       // スライドショーの変化 ("fade" or "slide")
       transition : 'slide',
       // 変化に係る時間(ミリ秒)
-      speed : 1500,
+      speed : 2000,
       // easingの種類
       easing : 'easeOutQuart',
       // ナビゲーションの形("block" or "bubble")
-      navType : 'bubble',
+      navType : 'block',
       // 子要素の種類("div" or "img")
       childrenElementType : 'div',
       // ナビゲーション矢印の表示(trueで表示)
@@ -41,7 +41,7 @@ document.addEventListener("turbolinks:load", function(){
       // スライドショーの自動再生(falseで自動再生なし)
       autoPlay : true,
       // 自動再生時のスライド切替間隔(ミリ秒)
-      autoPlayDuration : 3000,
+      autoPlayDuration : 4000,
       // キーボードの矢印キーによるスライド送りの設定(trueで有効)
       keyboardOnAlways : true,
       // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
