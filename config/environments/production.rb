776ed604bc5d1619a3089b20c43027ddc8e0aecc
smuskit <k.shimouse@gmail.com>
2019-02-23 12:51:02 +0000

    @media(max-width:990) Adjust the size and position of characters of background

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,8 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
+
+  # 本番環境でメールを送受信するための記述（お問い合わせフォーム）
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
